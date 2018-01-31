d1df2362fcaa456dc8c755ba08f0d9a54396b365
zhuzhongwei <1510331524@qq.com>
2018-01-31 10:00:16 +0800

    OK

--- sports-common/src/main/java/com/xlf/common/enums/RacingSeatEnum.java
+++ sports-common/src/main/java/com/xlf/common/enums/RacingSeatEnum.java
@@ -4,11 +4,16 @@ package com.xlf.common.enums;
  * 是否开奖
  */
 public enum RacingSeatEnum {
-    ONE(1, "个位开奖号"),
-    TWO(2, "十位开奖号"),
-    THREE(3, "百位开奖号"),
-    FOURE(4, "千位开奖号"),
-    FIVE(5, "万位开奖号");
+    ONE(1, "第一名开奖号"),
+    TWO(2, "第二名开奖号"),
+    THREE(3, "第三名开奖号"),
+    FOURE(4, "第四名开奖号"),
+    FIVE(5, "第五名开奖号"),
+    SIX(6, "第六名开奖号"),
+    SEVEN(7, "第七名开奖号"),
+    EIGHT(8, "第八名开奖号"),
+    NINE(9, "第九名开奖号"),
+    TEN(10, "第十名开奖号");
 
     RacingSeatEnum(Integer code, String name) {
         this.code = code;
