e8faaed1308d606298993464621125ed2b56a160
yyr <1091945691@qq.com>
2018-02-07 16:36:17 +0800

    ok

--- sports-app/src/main/java/com/xlf/app/controller/TimeBettingController.java
+++ sports-app/src/main/java/com/xlf/app/controller/TimeBettingController.java
@@ -78,7 +78,7 @@ public class TimeBettingController {
             String currentDate = DateTimeUtil.formatDate (new Date (), DateTimeUtil.PATTERN_YYYYMMDD);
             //本期期号
             String historyIssuNo = currentDate + "-" + Integer.valueOf (intervalPo.getIssueNo ());
-            String nextIssuNo = currentDate + Integer.valueOf (intervalPo.getIssueNo () + 1);
+            String nextIssuNo = currentDate + "-" + Integer.valueOf (intervalPo.getIssueNo () + 1);
             //本期投注截止时间
             String endDateStr = DateTimeUtil.formatDate (new Date (), DateTimeUtil.PATTERN_YYYY_MM_DD) + " " + hhmm;
             Date endDate = DateTimeUtil.parseDateFromStr (endDateStr, DateTimeUtil.PATTERN_YYYY_MM_DD_HH_MM);
