d4efefbe003b61a983aa7e4c8f4ac76deb307562
zhuzhongwei <1510331524@qq.com>
2018-03-08 15:59:09 +0800

    OK

--- sports-app/src/main/java/com/xlf/app/controller/RecordController.java
+++ sports-app/src/main/java/com/xlf/app/controller/RecordController.java
@@ -90,6 +90,100 @@ public class RecordController {
     }
 
 
+    /**
+     * 用户时时彩投注详细流水记录
+     */
+    @GetMapping(value = "/timeBettingRecord")
+    public RespBody timeBettingRecord(HttpServletRequest request, Paging paging, String businessNumber) {
+        RespBody respBody = new RespBody ();
+        try {
+            //根据用户id获取用户信息
+            List<AppTimeBettingPo> list = null;
+            //检验用户是否登录
+            AppUserPo appUserPo = commonService.checkToken ();
+            if (StringUtils.isEmpty (businessNumber)) {
+                paging.setTotalCount (0);
+                respBody.add (RespCodeEnum.ERROR.getCode (), AppMessage.PARAM_ERROR, "参数不合法");
+            }
+
+            //获取总记录数量
+            int total = appTimeBettingService.recordListTotal (appUserPo.getId (), businessNumber);
+            if (total > 0) {
+                list = appTimeBettingService.findRecordList (appUserPo.getId (), businessNumber, paging);
+            }
+            //返回前端总记录
+            paging.setTotalCount (total);
+            respBody.add (RespCodeEnum.SUCCESS.getCode (), "获取用户记录成功", paging, list);
+        } catch (CommException ex) {
+            respBody.add (RespCodeEnum.ERROR.getCode (), ex.getMessage ());
+        } catch (Exception ex) {
+            respBody.add (RespCodeEnum.ERROR.getCode (), "获取用户记录失败");
+            LogUtils.error ("获取用户记录失败！", ex);
+        }
+        return respBody;
+    }
+
+    /**
+     * 用户北京赛车投注详细流水记录
+     */
+    @GetMapping(value = "/racingBettingRecord")
+    public RespBody racingBettingRecord(HttpServletRequest request, Paging paging, String businessNumber) {
+        RespBody respBody = new RespBody ();
+        try {
+            //根据用户id获取用户信息
+            List<AppRacingBettingPo> list = null;
+            //检验用户是否登录
+            AppUserPo appUserPo = commonService.checkToken ();
+            if (StringUtils.isEmpty (businessNumber)) {
+                paging.setTotalCount (0);
+                respBody.add (RespCodeEnum.ERROR.getCode (), AppMessage.PARAM_ERROR, "参数不合法");
+            }
+
+            //获取总记录数量
+            int total = appRacingBettingService.recordListTotal (appUserPo.getId (), businessNumber);
+            if (total > 0) {
+                list = appRacingBettingService.findRecordList (appUserPo.getId (), businessNumber, paging);
+            }
+            //返回前端总记录
+            paging.setTotalCount (total);
+            respBody.add (RespCodeEnum.SUCCESS.getCode (), "获取用户记录成功", paging, list);
+        } catch (CommException ex) {
+            respBody.add (RespCodeEnum.ERROR.getCode (), ex.getMessage ());
+        } catch (Exception ex) {
+            respBody.add (RespCodeEnum.ERROR.getCode (), "获取用户记录失败");
+            LogUtils.error ("获取用户记录失败！", ex);
+        }
+        return respBody;
+    }
+
+
+    /**
+     * 用户流水记录
+     */
+    @GetMapping(value = "/withdrawRecord")
+    public RespBody withdrawRecord(HttpServletRequest request, Paging paging) {
+        RespBody respBody = new RespBody ();
+        try {
+            //根据用户id获取用户信息
+            List<DrawRecordVo> list = Collections.emptyList ();
+            //检验用户是否登录
+            AppUserPo appUserPo = commonService.checkToken ();
+            //获取总记录数量
+            int total = appWithDrawService.drawRecordTotal (appUserPo.getId ());
+            if (total > 0) {
+                list = appWithDrawService.drawRecordList (appUserPo.getId (), paging);
+            }
+            //返回前端总记录
+            paging.setTotalCount (total);
+            respBody.add (RespCodeEnum.SUCCESS.getCode (), msgUtil.getMsg (AppMessage.GET_RECORD_SUCCESS, "获取用户记录成功"), paging, list);
+        } catch (CommException ex) {
+            respBody.add (RespCodeEnum.ERROR.getCode (), ex.getMessage ());
+        } catch (Exception ex) {
+            respBody.add (RespCodeEnum.ERROR.getCode (), msgUtil.getMsg (AppMessage.GET_RECORD_FAILE, "获取用户记录失败"));
+            LogUtils.error ("获取用户记录失败！", ex);
+        }
+        return respBody;
+    }
 
 
 }
