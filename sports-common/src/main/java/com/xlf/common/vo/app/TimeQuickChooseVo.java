f93cd8954b626207f018ca0d6386c5fc2eddc45a
zhuzhongwei <1510331524@qq.com>
2018-03-10 16:27:35 +0800

    OK

---
+++ sports-common/src/main/java/com/xlf/common/vo/app/TimeQuickChooseVo.java
@@ -0,0 +1,36 @@
+package com.xlf.common.vo.app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TimeQuickChooseVo {
+    private Integer betType;
+    private Integer multiple;
+    private Map<Integer,String> map=new HashMap<> ();
+
+    public Integer getBetType() {
+        return betType;
+    }
+
+    public void setBetType(Integer betType) {
+        this.betType = betType;
+    }
+
+    public Integer getMultiple() {
+        return multiple;
+    }
+
+    public void setMultiple(Integer multiple) {
+        this.multiple = multiple;
+    }
+
+    public Map<Integer, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<Integer, String> map) {
+        this.map = map;
+    }
+}
