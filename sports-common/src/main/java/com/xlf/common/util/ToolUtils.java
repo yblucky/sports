7ef7a62d138e9351c6292dfcded17b77de59d2d9
zhuzhongwei <1510331524@qq.com>
2018-03-13 17:21:55 +0800

    ok

--- sports-common/src/main/java/com/xlf/common/util/ToolUtils.java
+++ sports-common/src/main/java/com/xlf/common/util/ToolUtils.java
@@ -357,10 +357,10 @@ public class ToolUtils {
             }
             List<String> same = new ArrayList<> ();
             for (String nums : list) {
-                Pattern p = Pattern.compile ("[^0-9]]");
+                Pattern p = Pattern.compile ("[^0-9]");
                 Matcher m = p.matcher (nums);
-                String num = m.replaceAll ("").trim ();
-                for (int i = 0; i < nums.length (); i++) {
+                String num = m.replaceAll (" ").trim ();
+                for (int i = 0; i < num.length (); i++) {
                     if (nums.charAt (0) == nums.charAt (1)) {
                         same.add (nums);
                     }
@@ -371,7 +371,7 @@ public class ToolUtils {
                 return same;
             }
             if (kindType == 3) {
-                list.retainAll (same);
+                list.removeAll (same);
                 return list;
             }
         }
@@ -388,25 +388,7 @@ public class ToolUtils {
             System.out.println (s);
         }
 
-        String strInput = "3a7s10@5d2a6s17s56;33";
-        String regEx = "[^0-9]";//匹配指定范围内的数字
 
-        //Pattern是一个正则表达式经编译后的表现模式
-        Pattern p = Pattern.compile (regEx);
-
-        // 一个Matcher对象是一个状态机器，它依据Pattern对象做为匹配模式对字符串展开匹配检查。
-        Matcher m = p.matcher (strInput);
-
-        //将输入的字符串中非数字部分用空格取代并存入一个字符串
-        String string = m.replaceAll ("").trim ();
-        System.out.println ("sss "+string);
-        //以空格为分割符在讲数字存入一个字符串数组中
-        String[] strArr = string.split (" ");
-
-        //遍历数组转换数据类型输出
-        for (String s : strArr) {
-            System.out.println (Integer.parseInt (s));
-        }
 //        String s="42092";
 //        List<String> list = oneLotteryList (s);
 //        List<String> list = twoLotteryList (s);
