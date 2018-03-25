23880af39f1e3f9a264dacb48846e6df84934929
zhuzhongwei <1510331524@qq.com>
2018-03-25 18:35:04 +0800

    OK

--- sports-task/src/main/java/com/xlf/task/app/ClearToddyReturnWaterScheduleTask.java
+++ sports-task/src/main/java/com/xlf/task/app/ClearToddyReturnWaterScheduleTask.java
@@ -1,14 +1,18 @@
 package com.xlf.task.app;
 
 
+import com.xlf.common.po.AppRacingLotteryPo;
 import com.xlf.common.po.AppTimeBettingPo;
 import com.xlf.common.po.AppTimeLotteryPo;
+import com.xlf.server.app.AppRacingLotteryService;
 import com.xlf.server.app.AppTimeBettingService;
 import com.xlf.server.app.AppTimeLotteryService;
 import com.xlf.server.app.AppUserService;
+import com.xlf.server.web.ParameterService;
 import com.xlf.server.web.SysUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -19,14 +23,23 @@ public class ClearToddyReturnWaterScheduleTask extends BaseScheduleTask {
     private SysUserService sysUserService;
     @Resource
     private AppUserService appUserService;
+    @Resource
+    private ParameterService parameterService;
+    @Resource
+    private AppRacingLotteryService appRacingLotteryService;
 
 
     @Override
     protected void doSpecificTask() {
         try {
             //清理用户的返水
-           sysUserService.updateClearTotayReturnWater ();
+            sysUserService.updateClearTotayReturnWater ();
             appUserService.updateClearTodayBettingAmoutTodayWiningAmout();
+            AppRacingLotteryPo po=appRacingLotteryService.loadAwardNumber();
+            if (po!=null && !StringUtils.isEmpty(po.getIssueNo())){
+                parameterService.updateParameterByName("yesterdayRacingIssuNo",po.getIssueNo());
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
